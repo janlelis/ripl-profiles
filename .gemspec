4338ae0af2392556122688fdd5a5eef84809f4f3
Jan Lelis <mail@janlelis.de>
2010-12-09 14:06:53 +0100

    ripl-profiles 0.1.0

---
+++ .gemspec
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+name = 'ripl-profiles'
+
+require 'rubygems' unless defined? Gem
+require File.dirname(__FILE__) + "/lib/ripl/profiles"
+ 
+Gem::Specification.new do |s|
+  s.name        = name
+  s.version     = Ripl::Profiles::VERSION
+  s.authors     = ["Jan Lelis"]
+  s.email       = "mail@janlelis.de"
+  s.homepage    = "http://github.com/janlelis/" + name
+  s.summary     = "This ripl plugin adds a --profile option to ripl that loads profile files in ~/.ripl/profiles."
+  s.description = "This ripl plugin adds a --profile option to ripl that loads profile files in ~/.ripl/profiles before starting ripl."
+  s.required_rubygems_version = ">= 1.3.6"
+  s.add_dependency 'ripl', '>= 0.2.6'
+  s.files = Dir.glob(%w[{lib,test}/**/*.rb bin/* [A-Z]*.{txt,rdoc} ext/**/*.{rb,c} **/deps.rip]) + %w{Rakefile .gemspec}
+  s.extra_rdoc_files = ["README.rdoc", "LICENSE.txt"]
+  s.license = 'MIT'
+end
---
+++ lib/ripl/profiles.rb
@@ -0,0 +1,80 @@
+require 'ripl'
+
+module Ripl::Profiles
+  VERSION = '0.1.0'
+
+  @loaded = []
+  
+  Ripl::Runner::OPTIONS << ['-p', '--profile NAME', 'Use a profile']
+
+  class << self
+    attr_reader :loaded
+
+    def available
+      Dir[ File.expand_path( File.join( Ripl.config[:profiles_prefix], '*' ) )].map{ |path|
+        File.basename( path ).sub( /#{File.extname(path)}$/,'' )
+      }
+    end
+
+    def load( names )
+      Array(names).each{ |name|
+        profile_path = File.expand_path( File.join( Ripl.config[:profiles_prefix], "#{ name }.rb" ) )
+        if File.exists? profile_path
+          Ripl::Runner.load_rc profile_path
+          puts "Loaded profile: #{ name }" if Ripl.config[:profiles_verbose]
+          @loaded << name
+        else
+          warn "ripl: Couldn't load the profile #{ name } at: #{ profile_path }"
+        end
+      }
+    end
+  end
+
+  # command shortcuts
+  module Commands
+    def available_profiles
+      Ripl::Profiles.list
+    end
+
+    def load_profile( name )
+      Ripl::Profiles.load name
+    end
+
+    def loaded_profiles
+      Ripl::Profiles.loaded
+    end
+  end
+
+  module Shell
+    # load default profile if non is set
+    def before_loop
+      super
+      if Ripl.config[:profiles_default] && Ripl::Profiles.loaded.empty?
+        Ripl::Profiles.load Ripl.config[:profiles_default]
+      end
+      if Ripl.config[:profiles_base]
+        Ripl::Profiles.load Ripl.config[:profiles_base]
+      end
+    end
+  end
+ 
+  module Runner
+    # add command line option
+    def parse_option( option, argv )
+      if option =~ /(?:-p|--profile)=?(.*)/
+        Ripl::Profiles.load ($1.empty? ? argv.shift.to_s : $1).split(':')
+      else
+        super
+      end
+    end
+  end
+end
+
+Ripl::Runner.send :extend,    Ripl::Profiles::Runner
+Ripl::Shell.send :include,    Ripl::Profiles::Shell
+Ripl::Commands.send :include, Ripl::Profiles::Commands
+
+Ripl.config[:profiles_prefix]  ||= "~/.ripl/profiles/"
+Ripl.config[:profiles_verbose] ||= false
+
+# J-_-L
